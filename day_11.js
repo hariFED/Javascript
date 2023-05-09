97191730733cad15cc85a3a1efb9c071a0943153
Hariharan <109363771+hariFED@users.noreply.github.com>
2023-05-09 23:13:46 +0530

    Update day_11.js

--- day_11.js
+++ day_11.js
@@ -56,3 +56,43 @@ function getCount(objects) {
 
 }
 getCount([{ x: 1, y: 2 }, { x: 2, y: 2 }, { x: 2, y: 1 }, { x: 1, y: 1 }])
+
+
+// this challenge, we practice implementing inheritance and use JavaScript prototypes to add a new method to an existing prototype. Check out the attached Classes tutorial to refresh what we've learned about these topics.
+
+// Task
+
+// We provide the implementation for a Rectangle class in the editor. Perform the following tasks:
+
+// Add an area method to Rectangle's prototype.
+// Create a Square class that satisfies the following:
+// It is a subclass of Rectangle.
+// It contains a constructor and no other methods.
+// It can use the Rectangle class' area method to print the area of a Square object.
+
+// Rectangle class
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
+
+// Add area method to Rectangle prototype
+Rectangle.prototype.area = function () {
+    return this.w * this.h;
+}
+
+// Square class
+class Square extends Rectangle {
+    constructor(s) {
+        super(s, s);
+    }
+}
+
+// Test the classes
+let rect = new Rectangle(3, 4);
+let square = new Square(3);
+
+console.log(rect.area()); // Output: 12
+console.log(square.area()); // Output: 9
