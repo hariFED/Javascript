096c58fe2852c9da12fb826cb9c355270cebfee7
Hariharan <109363771+hariFED@users.noreply.github.com>
2023-07-02 22:50:42 +0530

    nothing

--- day_10.js
+++ day_10.js
@@ -285,11 +285,7 @@ function categorizeCountries(arr, pattern) {
     console.log(tempLand)
 }
 categorizeCountries(countriesadded, 'Aus')
-// Create a function which return an array of objects, which is the letter and the number of times the letter use to start with a name of a country.
 
-// Declare a getFirstTenCountries function and return an array of ten countries. Use different functional programming to work on the countries.js array
-// Declare a getLastTenCountries function which which returns the last ten countries in the countries array.
-// Find out which letter is used many times as initial for a country name from the countries array (eg. Finland, Fiji, France etc)
 
 
 
---
+++ day_12.js
@@ -0,0 +1,174 @@
+
+const countriesadded = [
+    {
+        id: 'AF',
+        name: 'Afghanistan',
+        continent: 'Asia',
+        currencyId: 'AFN',
+        phoneCode: '93',
+        flag: '🇦🇫'
+    },
+    {
+        id: 'AX',
+        name: 'Åland Islands',
+        continent: 'Europe',
+        currencyId: 'EUR',
+        phoneCode: '35818',
+        flag: '🇦🇽'
+    },
+    {
+        id: 'AL',
+        name: 'Albania',
+        continent: 'Europe',
+        currencyId: 'ALL',
+        phoneCode: '355',
+        flag: '🇦🇱'
+    },
+    {
+        id: 'DZ',
+        name: 'Algeria',
+        continent: 'Africa',
+        currencyId: 'DZD',
+        phoneCode: '213',
+        flag: '🇩🇿'
+    },
+    {
+        id: 'AS',
+        name: 'American Samoa',
+        continent: 'Australia',
+        currencyId: 'USD',
+        phoneCode: '1684',
+        flag: '🇦🇸'
+    },
+    {
+        id: 'AD',
+        name: 'Andorra',
+        continent: 'Europe',
+        currencyId: 'EUR',
+        phoneCode: '376',
+        flag: '🇦🇩'
+    },
+    {
+        id: 'AO',
+        name: 'Angola',
+        continent: 'Africa',
+        currencyId: 'AOA',
+        phoneCode: '244',
+        flag: '🇦🇴'
+    },
+    {
+        id: 'AI',
+        name: 'Anguilla',
+        continent: 'North America',
+        currencyId: 'XCD',
+        phoneCode: '1264',
+        flag: '🇦🇮'
+    },
+    {
+        id: 'AQ',
+        name: 'Antarctica',
+        continent: 'Antarctica',
+        currencyId: '-/-',
+        phoneCode: '672',
+        flag: '🇦🇶'
+    },
+    {
+        id: 'AG',
+        name: 'Antigua And Barbuda',
+        continent: 'North America',
+        currencyId: 'XCD',
+        phoneCode: '1268',
+        flag: '🇦🇬'
+    },
+    {
+        id: 'AR',
+        name: 'Argentina',
+        continent: 'South America',
+        currencyId: 'ARS',
+        phoneCode: '54',
+        flag: '🇦🇷'
+    },
+    {
+        id: 'AM',
+        name: 'Armenia',
+        continent: 'Asia',
+        currencyId: 'AMD',
+        phoneCode: '374',
+        flag: '🇦🇲'
+    },
+    {
+        id: 'AW',
+        name: 'Aruba',
+        continent: 'North America',
+        currencyId: 'ANG',
+        phoneCode: '297',
+        flag: '🇦🇼'
+    },
+    {
+        id: 'AC',
+        name: 'Ascension Island',
+        continent: 'Africa',
+        currencyId: 'SHP',
+        phoneCode: '247',
+        flag: '🇦🇨'
+    },
+    {
+        id: 'AU',
+        name: 'Australia',
+        continent: 'Australia',
+        currencyId: 'AUD',
+        phoneCode: '61',
+        flag: '🇦🇺'
+    },
+    {
+        id: 'AT',
+        name: 'Austria',
+        continent: 'Europe',
+        currencyId: 'EUR',
+        phoneCode: '43',
+        flag: '🇦🇹'
+    },
+
+    {
+        id: 'BS',
+        name: 'Bahamas',
+        continent: 'North America',
+        currencyId: 'BSD',
+        phoneCode: '1242',
+        flag: '🇧🇸'
+    },
+    {
+        id: 'BH',
+        name: 'Bahrain',
+        continent: 'Asia',
+        currencyId: 'BHD',
+        phoneCode: '973',
+        flag: '🇧🇭'
+    },
+    {
+        id: 'BD',
+        name: 'Bangladesh',
+        continent: 'Asia',
+        currencyId: 'BDT',
+        phoneCode: '880',
+        flag: '🇧🇩'
+    }
+]
+// Create a function which return an array of objects, which is the letter and the number of times the letter use to start with a name of a country.
+
+function arrayOfobj(){
+
+let startLetter = "A"
+const findLetters = countriesadded.find(startLetter=>startLetter[n][
+    
+])
+
+
+
+
+}
+
+
+// Declare a getFirstTenCountries function and return an array of ten countries. Use different functional programming to work on the countries.js array
+// Declare a getLastTenCountries function which which returns the last ten countries in the countries array.
+// Find out which letter is used many times as initial for a country name from the countries array (eg. Finland, Fiji, France etc).//
